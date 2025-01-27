b86fb6f8a5b57b3930b28ed41d743831361e3757
asrahim88 <tasmimhasan1997@gmail.com>
2025-01-27 21:36:08 +0600

    updated

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'  # Adjust this as needed
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
